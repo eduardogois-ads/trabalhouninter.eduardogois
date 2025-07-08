72ac0e7a9b2a1f2dd229c8b6354c72124526b4d2
Eduardo Gois <eduardogois.ads@outlook.com>
2025-07-07 21:31:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+  const elemento = document.getElementById('mensagem');
+  elemento.textContent = "É um imenso prazer iniciar minha jornada na tecnologia com a UNINTER!";
+}
